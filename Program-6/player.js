57df4775c963b80b9539f0abe6f2ea649d5a8d03
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-07-01 22:57:43 -0500

    test

--- Program-6/player.js
+++ Program-6/player.js
@@ -59,9 +59,7 @@ var aPlayer = function(index, game, proxyServer){
         player.body.velocity.x = 0;
         player.body.velocity.y = 0;
 
-        console.log("hello??");
-
-        if (cursor.left){
+        if (upKey.isDown){
             player.body.velocity.x -= 100;
         }
         if (cursor.right){
