3a311bee40e1259c92a38906d0bc883d444a5c2f
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-07-03 18:12:32 -0500

    t

--- Program-6/player.js
+++ Program-6/player.js
@@ -246,9 +246,9 @@ var aNPC = function(index, myState, game, proxyServer){
 
         game.global.easystar.findPath(npcTile.x, npcTile.y, playerTile.x, playerTile.y, function(newPath){
             path = newPath;
-
+            console.log(path, newPath);
             if (path.length){
-                console.log("ohhi");
+
                 if(npcTile.x > path[0].x && npcTile.y == path[0].y)
                     nextStep = 'L';
                 else if (npcTile.x < path[0].x && npcTile.y == path[0].y)
