d61ab6dbac9a2966ac0b0fc0edac2017f06123bf
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-07-02 21:39:45 -0500

    test

--- Program-6/main.js
+++ Program-6/main.js
@@ -159,8 +159,8 @@ function initMultiPlayer(game,globals){
         drawMap(thisMap);
 
         console.log(spawnLoc);
-        globals.player.sprite.x = spawnLoc.x * 20;
-        globals.player.sprite.y = spawnLoc.y * 20;
+        globals.player.sprite.x = spawnLoc.x * 10;
+        globals.player.sprite.y = spawnLoc.y * 10;
 
         console.log(globals.playerList);
 
