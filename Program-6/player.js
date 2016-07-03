24d934fb6819c40a2f8a8f4c2cb73add1575fa57
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-07-03 18:46:27 -0500

    t

--- Program-6/player.js
+++ Program-6/player.js
@@ -244,6 +244,7 @@ var aNPC = function(index, myState, game, proxyServer){
         var map = game.global.map;
         var npcTile = map.getTileWorldXY(npc.x, npc.y, 20, 20, 'level1');
         var playerTile = map.getTileWorldXY(game.global.player.sprite.x, game.global.player.sprite.y, 20, 20, 'level1');
+        console.log(npcTile.x, npcTile.y, playerTile.x, playerTile.y);
         game.global.easystar.findPath(npcTile.x, npcTile.y, playerTile.x, playerTile.y, function(newPath){
             console.log(newPath);
             path = newPath;
