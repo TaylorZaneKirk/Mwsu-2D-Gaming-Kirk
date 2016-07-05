a1838e61b76ebca06dc505ee93f2c4d1c6b5a61b
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-07-05 00:13:38 -0500

    map gen

--- Program-6/server.js
+++ Program-6/server.js
@@ -22,9 +22,9 @@ var COLS = 40;
 
 //map steps for generation
 var numberOfSteps = 5; //How many times will we pass over the map
-var deathLimit = 4; //Least number of neighbours required to live
+var deathLimit = 3; //Least number of neighbours required to live
 var birthLimit = 5; //Greateast number of neighbours before cell dies
-var chanceToStartAlive = 0.47;  //chance of being generated as alive
+var chanceToStartAlive = 0.487;  //chance of being generated as alive
 
 //attach eureca.io to our http server
 eurecaServer.attach(server);
