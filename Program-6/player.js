01be15ecb99a5b63960dad7ae4e8358f26bfb10b
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-07-08 12:19:49 -0500

    debugging

--- Program-6/player.js
+++ Program-6/player.js
@@ -63,7 +63,7 @@ var aPlayer = function(index, game, proxyServer){
             game.physics.arcade.overlap(player, game.global.warps[w], function(){
                 player.x = 0;
                 player.y = 0;
-
+                console.log(game.global.warps[w]);
                 proxy.moveMap(player_id, game.global.warps[w].dest);
             });
         }
