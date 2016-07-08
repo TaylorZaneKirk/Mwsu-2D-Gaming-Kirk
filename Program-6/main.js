213ee008c4ae83028bd7e2351a49fde8845bb552
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-07-08 13:24:13 -0500

    test

--- Program-6/main.js
+++ Program-6/main.js
@@ -237,6 +237,7 @@ function drawMap(myMap) {
     for (var y = 0; y < ROWS; y++)
         for (var x = 0; x < COLS; x++) {
             var thisTile;
+            map.putTile(3, x, y, 'level1');
             if (myMap[y][x]){
                 map.putTile(3, x, y, 'level1')
                 map.putTile(0, x, y, 'collisions');
