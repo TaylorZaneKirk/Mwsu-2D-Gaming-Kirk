315e78adbc1cc45269ed4b30b43fbb23bc9fafc7
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-07-17 13:20:15 -0500

    comment stuff

--- webpage/index.php
+++ webpage/index.php
@@ -1,12 +1,12 @@
 
 <html>
 
-    <!-->
+    <!--
         Name: Taylor Kirk
         Professor: Dr. Passos
         Class: Web-App Security
         Date: 7/17/16
-    </-->
+    -->
 
     <head>
 
@@ -16,10 +16,10 @@
 
     <body>
 
-        <!-->
+        <!--
             Basic user interaction. Enter your name,
             browser "remembers" your name and greets you
-        </-->
+        -->
 
         <form action ="" method="get">
             What's your name?
