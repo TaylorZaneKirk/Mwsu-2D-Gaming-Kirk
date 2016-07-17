17a2562eab0f938318c1062a449e731eae963bb7
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-07-17 11:56:20 -0500

    test

--- webpage/index.php
+++ webpage/index.php
@@ -10,9 +10,14 @@
 
         What's your name?
         <input type="text" name="uname" id="uname" size="30">
+        <input type="submit" name="info">
 
         <?php
             echo "hello world";
+            if ( !empty($_POST['uname'])){
+                $name = $_POST['uname'];
+            }
+            echo "Your name is:" + $name
         ?>
 
 
