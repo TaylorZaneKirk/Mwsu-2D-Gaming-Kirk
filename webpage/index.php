94e5e8cc1d2b940ab94bbc388e0da292ba15ac5f
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-07-26 09:46:35 -0500

    Assignment #4

--- webpage/index.php
+++ webpage/index.php
@@ -2,14 +2,14 @@
     Name: Taylor Kirk
     Professor: Dr. Passos
     Class: Web-App Security
-    Date: 7/17/16
+    Date: 7/25/16
 -->
 
 <html>
 
     <head>
 
-        <title>Assignment 2</title>
+        <title>Assignment 4</title>
 
     </head>
 
@@ -20,24 +20,34 @@
             browser "remembers" your name and greets you
         -->
 
-        <form action ="" method="get">
-            What's your name?
-            <input type="text" name="uname" id="uname" size="30">
-            <input type="submit" name="info">
-        </form>
-
-
-        <?php
-
-            if ( !empty($_GET['uname'])){
-                $name = $_GET['uname'];
-                echo "Your name is:" . $name;
-            }
-            else{
-                echo "Please enter your name above.";
-            }
-
-        ?>
+        <FORM name="Calculator">
+            <table border="4">  <!--OUTER BORDER OF CALCULATOR, PANELS-->
+                <tr>   <!--TOP PANEL------>
+                    <td colspan="2">A Simple JavaScript Calculator<br>
+                        <input type="text" maxlength="40" size="30" name="Display">
+                    </td>
+                </tr>  <!--END TOP PANEL--->
+                <tr>
+                    <td> <!--LEFT PANEL------>
+                        <table>
+                            <tr>
+                                <td><input type="button" value="7"></td>
+                                <td><input type="button" value="8"></td>
+                                <td><input type="button" value="9"></td>
+                            </tr>
+                            <tr>
+                                {all other rows of digits are similar..}
+                            </tr>
+                        </table>
+                    </td><!--END LEFT PANEL-->
+                    <td> <!--RIGHT PANEL----->
+                        <table>
+                            {right panel is similar in overall structure to left}
+                        </table>
+                    </td><!--END RIGHT PANEL-->
+                </tr>
+            </table>
+</FORM>
 
 
     </body>
