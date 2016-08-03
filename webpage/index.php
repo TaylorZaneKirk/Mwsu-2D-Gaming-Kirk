91e2160fc49ce36de63a2ff927682edb51f6a571
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-08-03 15:22:37 -0500

    halp

--- webpage/index.php
+++ webpage/index.php
@@ -32,7 +32,7 @@ Date: 7/25/16
             //errorCodes[0] = Empty Input
             //errorCodes[1] = Alpha chars
             //errorCodes[2] = Special chars
-            $errorCodes = array(false, false, false, false);
+            //$errorCodes = array(false, false, false, false);
 
             $pdo = new mysqli($host, $user, $pass, $db);
 //
@@ -54,16 +54,16 @@ Date: 7/25/16
             else{
                 if (preg_match("/[a-zA-Z]/", $account, $match)){
                     //Match any alpha-character input
-                    $errorCodes[1] = true;
+                    //$errorCodes[1] = true;
                 }
-                elseif (preg_match("/[\W]+/", $account, $match)){
-                    //Match any 'non-word' input (flags special chars)
-                    $errorCodes[2] = true;
-                }
-                elseif (!preg_match("/[0-9]/", $account, $match)){
-                    //Trigger if any input is explicitly NOT a number
-                    $errorCodes[3] = true;
-                }
+//                elseif (preg_match("/[\W]+/", $account, $match)){
+//                    //Match any 'non-word' input (flags special chars)
+//                    $errorCodes[2] = true;
+//                }
+//                elseif (!preg_match("/[0-9]/", $account, $match)){
+//                    //Trigger if any input is explicitly NOT a number
+//                    $errorCodes[3] = true;
+//                }
                 else{
                     echo 'hello, ' . $account . '<br>';
 
@@ -82,18 +82,18 @@ Date: 7/25/16
                 }
             }
 
-            if(errorCodes[0]){
-                echo 'Please Enter Account Number Above';
-            }
-            if(errorCodes[1]){
-                echo 'Request Refused: No Alpha-Characters Allowed';
-            }
-            if(errorCodes[2]){
-                echo 'Request Refused: No Special Characters Allowed';
-            }
-            if(errorCodes[3]){
-                echo 'Request Refused: Input must only contain Numerical Characters';
-            }
+//            if(errorCodes[0]){
+//                echo 'Please Enter Account Number Above';
+//            }
+//            if(errorCodes[1]){
+//                echo 'Request Refused: No Alpha-Characters Allowed';
+//            }
+//            if(errorCodes[2]){
+//                echo 'Request Refused: No Special Characters Allowed';
+//            }
+//            if(errorCodes[3]){
+//                echo 'Request Refused: Input must only contain Numerical Characters';
+//            }
 
 
             $pdo->close();
