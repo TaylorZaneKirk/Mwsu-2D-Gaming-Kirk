99241451842f983ca844107efc402908edc1ca2b
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-08-03 12:10:01 -0500

    trying different function

--- webpage/index.php
+++ webpage/index.php
@@ -55,7 +55,7 @@ Date: 7/25/16
                 echo 'hello, ' . $account;
 
                 $preparedQuery = $pdo->prepare('SELECT * FROM accounts WHERE account_id = :account');
-                $preparedQuery->bindParam(':account', $account);
+                $preparedQuery->bindValue(':account', $account);
                 $preparedQuery->execute();
                 $result = $preparedQuery->get_result();
                 echo 'Your Result: ' . $result;
