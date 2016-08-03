2bbeb3f600abdeed2fcd0fe48c674bc6ed128483
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-08-03 15:10:35 -0500

    More validation

--- webpage/index.php
+++ webpage/index.php
@@ -29,6 +29,11 @@ Date: 7/25/16
             $pass = '1VT2yQtVjX';
             $charset = 'utf8';
 
+            //errorCodes[0] = Empty Input
+            //errorCodes[1] = Alpha chars
+            //errorCodes[2] = Special chars
+            $errorCodes = array(false, false, false, false);
+
             $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
             $opt = [
                 PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
@@ -49,11 +54,21 @@ Date: 7/25/16
             }
 
            if (empty($account)){
-               echo 'Please Enter Account Number Above';
+               //input is empty
+               $errorCodes[0] = true;
             }
             else{
                 if (preg_match("/[a-zA-Z]/", $account, $match)){
-                    echo 'Request Refused: No Alpha-Characters Allowed';
+                    //Match any alpha-character input
+                    $errorCodes[1] = true;
+                }
+                elseif (preg_match("/[\W]+/", $account, $match)){
+                    //Match any 'non-word' input (flags special chars)
+                    $errorCodes[2] = true;
+                }
+                elseif (!preg_match("/[0-9]/")){
+                    //Trigger if any input is explicitly NOT a number
+                    $errorCodes[3] = true;
                 }
                 else{
                     echo 'hello, ' . $account . '<br>';
@@ -72,6 +87,21 @@ Date: 7/25/16
                     }
                 }
             }
+
+            if(errorCodes[0]){
+                echo 'Please Enter Account Number Above';
+            }
+            if(errorCodes[1]){
+                echo 'Request Refused: No Alpha-Characters Allowed';
+            }
+            if(errorCodes[2]){
+                echo 'Request Refused: No Special Characters Allowed';
+            }
+            if(errorCodes[3]){
+                echo 'Request Refused: Input must only contain Numerical Characters';
+            }
+
+
             $pdo->close();
         ?>
 
