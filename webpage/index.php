80f9c7fa4db63cee668382977accd7fc65d89202
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-08-03 12:18:52 -0500

    m

--- webpage/index.php
+++ webpage/index.php
@@ -56,6 +56,8 @@ Date: 7/25/16
 
                 $preparedQuery = $pdo->prepare('SELECT * FROM accounts WHERE account_id = ?');
                 $preparedQuery->bindParam(1, $account);
+                echo 'Hi! ' . $preparedQuery->debugDumpParams();
+
                 $preparedQuery->execute();
                 $result = $preparedQuery->get_result();
                 echo 'Your Result: ' . $result;
