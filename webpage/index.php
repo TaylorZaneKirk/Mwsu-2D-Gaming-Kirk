828d4136631cc5167ba72c68e37fc35fa1dadff5
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-08-03 09:58:42 -0500

    testing db

--- webpage/index.php
+++ webpage/index.php
@@ -16,11 +16,21 @@ Date: 7/25/16
     <body>
 
         <?php
-            if (isset($_POST['account'])){
-                $account = $_POST['account'];
+            //Just test stuff
+        $db = new PDO('mysql:dbname=bank; host=127.0.0.1; charset=utf8', 'root', '1VT2yQtVjX');
+            $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); //disable emulation
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //should turn this off
+
+
+            $preparedQuery = $PDO->prepare('SELECT * FROM bank WHERE account_id = ?');
+            if ($preparedQuery->execute($_GET['account']){
+                foreach($preparedQuery as $item){
+                    echo 'hi';
+                }
             }
             else{
-                echo '<form action="" method="post">
+                //Display form
+                echo '<form action="" method="get">
                     Please Enter your Account information:<br>
                     <input type="text" maxlength="40" size="30" name="AccountQuery" id="account"><br>
                     <input type="submit" value="Check" name="AccountInfo"><br>
