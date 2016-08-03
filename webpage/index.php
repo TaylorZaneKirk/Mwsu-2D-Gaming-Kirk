42601ad27d346d6debd797255245b5decb10705b
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-08-03 10:08:11 -0500

    debug

--- webpage/index.php
+++ webpage/index.php
@@ -23,12 +23,11 @@ Date: 7/25/16
 
 
             $preparedQuery = $PDO->prepare('SELECT * FROM bank WHERE account_id = ?');
-            if ($preparedQuery->execute($_GET['account']){
-                echo 'hi';
+//            if ($preparedQuery->execute($_GET['account']){
 //                foreach($preparedQuery as $item){
 //                    echo 'hi';
 //                }
-            }
+//            }
 //            else{
 //                //Display form
 //                echo '<form action="" method="get">
