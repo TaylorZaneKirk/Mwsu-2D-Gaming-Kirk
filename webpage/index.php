c4bdd3c9ee4c8de5f09cdd7e72f7763183798d50
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-08-03 10:15:34 -0500

    PDO rewrite

--- webpage/index.php
+++ webpage/index.php
@@ -17,9 +17,23 @@ Date: 7/25/16
 
         <?php
             //Just test stuff
-            $db = new PDO('mysql:dbname=bank; host=127.0.0.1; charset=utf8', 'root', '1VT2yQtVjX') or die("system down");
-            $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); //disable emulation
-            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //should turn this off
+            $host = '127.0.0.1';
+            $db   = 'bank';
+            $user = 'root';
+            $pass = '1VT2yQtVjX';
+            $charset = 'utf8';
+
+            $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+            $opt = [
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES   => false,
+            ];
+            $pdo = new PDO($dsn, $user, $pass, $opt);
+
+//            $db = new PDO('mysql:host=127.0.0.1; dbname=bank; charset=utf8', 'root', '1VT2yQtVjX')l
+//            $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); //disable emulation
+//            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //should turn this off
 
 
             $preparedQuery = $PDO->prepare('SELECT * FROM bank WHERE account_id = ?');
