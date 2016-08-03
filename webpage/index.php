5dd81a7375aa70c52db2d30c318f5846a460d6aa
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-08-03 15:15:30 -0500

    whoops

--- webpage/index.php
+++ webpage/index.php
@@ -34,12 +34,6 @@ Date: 7/25/16
             //errorCodes[2] = Special chars
             $errorCodes = array(false, false, false, false);
 
-            $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-            $opt = [
-                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                PDO::ATTR_EMULATE_PREPARES   => false,
-            ];
             $pdo = new mysqli($host, $user, $pass, $db);
 //
 //            $db = new PDO('mysql:host=127.0.0.1; dbname=bank; charset=utf8', 'root', '1VT2yQtVjX');
@@ -66,7 +60,7 @@ Date: 7/25/16
                     //Match any 'non-word' input (flags special chars)
                     $errorCodes[2] = true;
                 }
-                elseif (!preg_match("/[0-9]/")){
+                elseif (!preg_match("/[0-9]/", $account, $match)){
                     //Trigger if any input is explicitly NOT a number
                     $errorCodes[3] = true;
                 }
