7d056cc07f9bed5dd80aadd6abf6dadc3d50295d
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-08-03 10:58:21 -0500

    testing

--- webpage/index.php
+++ webpage/index.php
@@ -17,23 +17,23 @@ Date: 7/25/16
 
         <?php
             //Just test stuff
-            $host = '127.0.0.1';
-            $db   = 'bank';
-            $user = 'root';
-            $pass = '1VT2yQtVjX';
-            $charset = 'utf8';
+//            $host = '127.0.0.1';
+//            $db   = 'bank';
+//            $user = 'root';
+//            $pass = '1VT2yQtVjX';
+//            $charset = 'utf8';
+//
+//            $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+//            $opt = [
+//                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+//                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+//                PDO::ATTR_EMULATE_PREPARES   => false,
+//            ];
+//            $pdo = new PDO($dsn, $user, $pass, $opt);
 
-            $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-            $opt = [
-                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                PDO::ATTR_EMULATE_PREPARES   => false,
-            ];
-            $pdo = new PDO($dsn, $user, $pass, $opt);
-
-//            $db = new PDO('mysql:host=127.0.0.1; dbname=bank; charset=utf8', 'root', '1VT2yQtVjX')l
-//            $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); //disable emulation
-//            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //should turn this off
+            $db = new PDO('mysql:host=127.0.0.1; dbname=bank; charset=utf8', 'root', '1VT2yQtVjX')l
+            $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); //disable emulation
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //should turn this off
 
 
            if (isset($_GET['account']){
