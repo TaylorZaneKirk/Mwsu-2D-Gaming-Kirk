93102e22cecf8bcc3602664e173e8afdd713f44a
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-08-03 11:32:22 -0500

    syntactical stuff

--- webpage/index.php
+++ webpage/index.php
@@ -18,28 +18,28 @@ Date: 7/25/16
         <form action="index.php" method="GET">
             Please Enter your Account information:<br>
             <input type="text" maxlength="40" size="30" name="AccountQuery" id="account"><br>
-            <input type="submit" value="Check" name="AccountInfo"><br>
+            <input type="submit" name="AccountInfo"><br>
         </form>
 
         <?php
             //Just test stuff
-//            $host = '127.0.0.1';
-//            $db   = 'bank';
-//            $user = 'root';
-//            $pass = '1VT2yQtVjX';
-//            $charset = 'utf8';
-//
-//            $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-//            $opt = [
-//                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-//                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-//                PDO::ATTR_EMULATE_PREPARES   => false,
-//            ];
-//            $pdo = new PDO($dsn, $user, $pass, $opt);
+            $host = '127.0.0.1';
+            $db   = 'bank';
+            $user = 'root';
+            $pass = '1VT2yQtVjX';
+            $charset = 'utf8';
 
-            $db = new PDO('mysql:host=127.0.0.1; dbname=bank; charset=utf8', 'root', '1VT2yQtVjX');
-            $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); //disable emulation
-            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //should turn this off
+            $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+            $opt = [
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES   => false,
+            ];
+            $pdo = new PDO($dsn, $user, $pass, $opt);
+//
+//            $db = new PDO('mysql:host=127.0.0.1; dbname=bank; charset=utf8', 'root', '1VT2yQtVjX');
+//            $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); //disable emulation
+//            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //should turn this off
 
 
            if (!isset($_GET['account'])){
@@ -50,7 +50,7 @@ Date: 7/25/16
                 echo 'hello';
                 $account = $_GET['account'];
 
-                $preparedQuery = $PDO->prepare('SELECT * FROM accounts WHERE account_id = ?');
+                $preparedQuery = $pdo->prepare('SELECT * FROM accounts WHERE account_id = ?');
                 $preparedQuery->bind_param('s', $account);
 
                 $preparedQuery->execute();
