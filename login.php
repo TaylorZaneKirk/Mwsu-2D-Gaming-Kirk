8fcc3c0f8552e97ba2f31359cc99e3c73cd8d585
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-11-07 15:39:01 -0600

    test

--- login.php
+++ login.php
@@ -5,13 +5,18 @@
 	if($preparedQuery = $conn->prepare('SELECT password FROM userlogin WHERE username=?')){
 		$preparedQuery->bind_param('s', $user_name);  //'?' from above query becomes $account
 		$preparedQuery->execute();  //send the query to db
-		$preparedQuery->bind_result($user_data);  //the result of the query can
+		$preparedQuery->bind_result($user_pass);  //the result of the query can
 												//ONLY be placed in $user_data, which needs to be shared between all activities
 		//get result of query
 		if($preparedQuery->fetch()){
 			//fetch was good, do the displays
-			//List working-account
-			echo 'login success !!!!! Welcome ' . $user_name;
+			//Test passwords
+            if($user_pass == $password)
+			    echo 'login success !!!!! Welcome ' . $user_name;
+            else{
+                //User exists, but wrong pass
+                echo 'Request Refused: Please check password and try again';
+            }
 		}
 		else{
 			//input was good, but record does not exist in db
