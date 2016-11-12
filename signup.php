986378360abe56a633abc0dca105f83b9142f0ee
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-11-11 20:29:15 -0600

    testing

--- signup.php
+++ signup.php
@@ -16,9 +16,31 @@
     $uphone = '9784561236';
 
     //check if there is already an account associated with specified username or email
-    if($preparedQuery = $conn->prepare('SELECT * FROM numbusiouser WHERE username=? OR email=?')){
+    if($preparedQuery = $conn->prepare('SELECT uid FROM numbusiouser WHERE username=? OR email=?')){
         echo 'hello1';
-       
+        $preparedQuery->bind_param('ss', $uname, $uemail);
+        $preparedQuery->execute();
+        $preparedQuery->bind_result($matches);
+
+        if($preparedQuery->fetch()){
+            //match found, refuse
+            echo 'Error: Account with your chosen username or email already exists!';
+            $preparedQuery->close();
+        }
+        else{
+            //No record exists, add one...
+            if($preparedQuery = $conn->prepare('INSERT into nimbusiouser (username, firstname, lastname, password, email, phone) VALUES (?, ?, ?, ?, ?, ?)')){
+                echo 'hello2';
+                $preparedQuery->bind_param('ssssss', $uname, $fname, $lname, $upass, $uemail, $uphone); 
+                $preparedQuery->execute();
+
+                echo 'Row inserted';
+            }
+            else{
+                echo "Error: ".$conn->error;
+            }
+            $preparedQuery->close();
+        }
     }
     
     $conn.close();
