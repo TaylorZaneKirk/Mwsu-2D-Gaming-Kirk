67d8a9bba891053a1777de632bc74b17aa29f771
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-11-11 20:50:54 -0600

    Making sure I did not break it

--- signup.php
+++ signup.php
@@ -1,30 +1,21 @@
 <?php
     require "conn.php";
 
-    //$fname = $_POST["first_name"];
-    //$lname = $_POST["last_name"];
-    //$uname = $_POST["user_name"];
-    //$upass = $_POST["password"];
-    //$uemail = $_POST["user_email"];
-    //$uphone = $_POST["user_phone"];
-
-    $fname = 'test1';
-    $lname = 'test1';
-    $uname = 'test4';
-    $upass = 'test1';
-    $uemail = 'test4@test.com';
-    $uphone = '9784561236';
+    $fname = $_POST["first_name"];
+    $lname = $_POST["last_name"];
+    $uname = $_POST["user_name"];
+    $upass = $_POST["password"];
+    $uemail = $_POST["user_email"];
+    $uphone = $_POST["user_phone"];
 
     //check if there is already an account associated with specified username or email
     if($preparedQuery = $conn->prepare('SELECT * FROM nimbusiouser WHERE username=? OR email=?')){
         $preparedQuery->bind_param('ss', $uname, $uemail);
         $preparedQuery->execute();
-        //$preparedQuery->bind_result($matches);
 
         if($preparedQuery->fetch()){
             //match found, refuse
             echo 'Error: Account with your chosen username or email already exists!';
-            $preparedQuery->close();
         }
         else{
             //No record exists, add one...
@@ -37,8 +28,8 @@
             else{
                 echo "Error: ".$conn->error;
             }
-            $preparedQuery->close();
         }
+        $preparedQuery->close();
     }
     else{
         echo "Error: ".$conn->error;
