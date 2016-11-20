4867544438a6f077eb8907a90008a2ade63b5046
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-11-20 17:26:51 -0600

    testing

--- fetchAccountInfo.php
+++ fetchAccountInfo.php
@@ -6,6 +6,7 @@
         $preparedQuery->bind_param('s', $user_name);
         $preparedQuery->execute();
         $preparedQuery->bind_result($res);
+        echo($res);
         $result = array();
         array_push($result, array("uid"=>$res['uid'],"firstname"=>$res['firstname'],"lastname"=>$res['lastname'],"email"=>$res['email'],"phone"=>$res['phone']));
         echo json_encode(array("result"=>$result));
