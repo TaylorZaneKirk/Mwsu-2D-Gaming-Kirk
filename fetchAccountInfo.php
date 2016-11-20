e24ec8bf7e1480442c2733e26543bde586563c30
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-11-20 17:18:56 -0600

    testing php

---
+++ fetchAccountInfo.php
@@ -0,0 +1,15 @@
+<?php
+    require "conn.php";
+    $user_name = 'vcmandalapu';
+
+    if($preparedQuery = $conn->prepare('SELECT uid, firstname, lastname, email, phone FROM nimbusiouser WHERE username=?')){
+        $preparedQuery->bind_param('s', $user_name);
+        $preparedQuery->execute();
+        $preparedQuery->bind_result($res);
+        $result = array();
+        array_push($result, array("uid"=>$res['uid'],"firstname"=>$res['firstname'],"lastname"=>$res['lastname'],"email"=>$res['email'],"phone"=>$res['phone']));
+        echo json_encode(array("result"=>$result));
+    }
+
+    $preparedQuery->close();
+?>
