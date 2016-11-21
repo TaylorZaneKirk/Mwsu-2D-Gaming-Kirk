ba69046c03679624603adf697dd5b7fdd49cb1f0
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-11-20 19:17:58 -0600

    test

--- fetchAccountInfo.php
+++ fetchAccountInfo.php
@@ -3,6 +3,7 @@
     $user_name = 'vcmandalapu';
 
     if($preparedQuery = $conn->prepare('SELECT uid, firstname, lastname, email, phone FROM nimbusiouser WHERE username=?')){
+        echo "true";
         $preparedQuery->bind_param('s', $user_name);
         $preparedQuery->execute();
         $preparedQuery->bind_result($r_uid, $r_firstname, $r_lastname, $r_email, $r_email, $r_phone);
