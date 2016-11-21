ff201eaf27d51e25c2004745fa475d6058cb6ce6
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-11-20 19:17:06 -0600

    test

--- fetchAccountInfo.php
+++ fetchAccountInfo.php
@@ -5,10 +5,10 @@
     if($preparedQuery = $conn->prepare('SELECT uid, firstname, lastname, email, phone FROM nimbusiouser WHERE username=?')){
         $preparedQuery->bind_param('s', $user_name);
         $preparedQuery->execute();
-        $preparedQuery->bind_result($res);
-        echo($res);
+        $preparedQuery->bind_result($r_uid, $r_firstname, $r_lastname, $r_email, $r_email, $r_phone);
+        echo($r_email);
         $result = array();
-        array_push($result, array("uid"=>$res['uid'],"firstname"=>$res['firstname'],"lastname"=>$res['lastname'],"email"=>$res['email'],"phone"=>$res['phone']));
+        array_push($result, array("uid"=>$r_uid,"firstname"=>$r_firstname,"lastname"=>$r_lastname,"email"=>$r_email,"phone"=>$r_phone));
         echo json_encode(array("result"=>$result));
     }
 
