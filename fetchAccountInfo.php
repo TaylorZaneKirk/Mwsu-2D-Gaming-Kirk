71826d7de44628adbcd54e5f15e4de70b245071f
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-11-20 19:29:29 -0600

    done

--- fetchAccountInfo.php
+++ fetchAccountInfo.php
@@ -8,6 +8,7 @@
         $preparedQuery->execute();
         $preparedQuery->bind_result($r_uid, $r_firstname, $r_lastname, $r_email, $r_phone);
         if($preparedQuery->fetch()){
+            $result = array();
             array_push($result, array("uid"=>$r_uid,"firstname"=>$r_firstname,"lastname"=>$r_lastname,"email"=>$r_email,"phone"=>$r_phone));
             echo json_encode(array("result"=>$result));
         }
