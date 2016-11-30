2370939c591642e133b03d5cb702753b30a8ed1e
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-11-30 16:34:24 -0600

    test

--- setstuff2.php
+++ setstuff2.php
@@ -42,8 +42,8 @@
     if($serviceid == 0){ //Is custom?
 
         //Check if this custom type already has a record
-        if($preparedQuery2 = $conn->prepare('SELECT serviceid FROM nimbusioservices WHERE servicename=?')){
-            $preparedQuery2->bind_param('s', $servicename);
+        if($preparedQuery2 = $conn->prepare('SELECT serviceid FROM nimbusioservices WHERE servicename=? AND servicedesc=?')){
+            $preparedQuery2->bind_param('ss', $servicename, $servicedesc);
             $preparedQuery2->execute();
             $preparedQuery2->store_result();
             $preparedQuery2->bind_result($newserviceid);
