5109deb8670d92e071b8317f98329f9c6584b846
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-11-30 15:39:51 -0600

    test

--- setstuff2.php
+++ setstuff2.php
@@ -54,7 +54,7 @@
             else{ //No match found, create new type
                 //Get the max value from the serviceid column of the nimbusioservices
 
-                if($preparedQuery3 = $conn->prepare('SELECT serviceid FROM nimbusioservices WHERE serviceid=(SELECT MAX(serviceid) from nimbusioservices)')){
+                if($preparedQuery3 = $conn->prepare('SELECT MAX(serviceid) AS Biggest FROM nimbusioservices')){
                     $preparedQuery3->execute();
                     $preparedQuery3->store_result();
                     $preparedQuery3->bind_result($maxType);
