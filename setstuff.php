1b3fbf57f106458879692fd13afc38dfb21a1aeb
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-11-30 12:33:02 -0600

    test

--- setstuff.php
+++ setstuff.php
@@ -1,65 +1,86 @@
 <?php
     require "conn.php";
 
-    $uid = $_GET['uid'];
-    //$ipAddress = $_GET['ipAddress'];
-    $serverType = $_GET['serverType'];
-    $serverName = $_GET['serverName'];
-    $serverDesc = $_GET['serverDesc'];
-    $serverRAM = $_GET['serverRAM'];
-    $serverProc = $_GET['serverProc'];
-    $serverSpace = $_GET['serverSpace'];
+    //$uid = $_GET['uid'];
+    //$IPaddress = $_GET['IPaddress'];
+    //$serviceid = $_GET['serviceid'];
+    //$servicename = $_GET['servicename'];
+    //$servicedesc = $_GET['servicedesc'];
+    //$serviceprice = $_GET['serviceprice'];
+    //$Memory = $_GET['Memory'];
+    //$Processor = $_GET['Processor'];
+    //$Storage = $_GET['Storage'];
 
-    //generate a random ipAddress string
-    $ipAddress = "".mt_rand(0,255).".".mt_rand(0,255).".".mt_rand(0,255).".".mt_rand(0,255);
+    $uid = 2;
+    //$IPaddress = $_GET['IPaddress'];
+    $serviceid = 1;
+    $servicename = "LAMP";
+    $servicedesc = "Linux, Apache, MySQL, PHP";
+    $serviceprice = 4.99;
+    $Memory = 512;
+    $Processor = "Xenon";
+    $Storage = 10240;
 
-    //User just bought a server, add their server to ServerTable
-    if($preparedQuery = $conn->prepare('INSERT INTO serverTable (uid, ipAddress, serverType) VALUES (?,?,?)')){
-        $preparedQuery->bind_param('isi', $uid, $ipAddress, $serverType);
+    //generate a random IPaddress string
+    $IPaddress = "".mt_rand(0,255).".".mt_rand(0,255).".".mt_rand(0,255).".".mt_rand(0,255);
+
+    //User just bought a server, add their server to userservers
+    if($preparedQuery = $conn->prepare('INSERT INTO userservers (uid, serviceid, IPaddress) VALUES (?,?,?)')){
+        $preparedQuery->bind_param('iis', $uid, $serviceid, $IPaddress);
         $preparedQuery->execute();
         //If using preset types, job is done. If user is using a custom type, first
         //  check to see if there is already a custom type on record that matches
         //  all configurations of THIS custom type. If there is a match, update
-        //  the serverType record on the ServerTable to use that matching type.
-        //  If there is no match returned, create a new serverType on the ServicesTable
-        //  and then update the serverType record on the ServerTable to reflect
-        //  the new serverType
+        //  the serviceid record on the userservers to use that matching type.
+        //  If there is no match returned, create a new serviceid on the nimbusioservices
+        //  and then update the serviceid record on the userservers to reflect
+        //  the new serviceid
+
+        echo "test1";
         
-        if($serverType == 0){ //Is custom?
+        if($serviceid == 0){ //Is custom?
 
             //Check if this custom type already has a record
-            if($preparedQuery = $conn->prepare('SELECT serverType FROM servicesTable WHERE serverName=? AND serverDesc=? AND serverProc=? AND serverRAM=? AND serverSpace=?')){
-                $preparedQuery->bind_param('sssss', $serverName, $serverDesc, $serverProc, $serverRAM, $serverSpace);
-                $preparedQuery->execute();
-                $preparedQuery->bind_result($newServerType);
+            if($preparedQuery2 = $conn->prepare('SELECT serviceid FROM nimbusioservices WHERE servicename=? AND servicedesc=? AND serviceprice=? AND Processor=? AND Memory=? AND Storage=?')){
+                $preparedQuery2->bind_param('ssisii', $servicename, $servicedesc, $serviceprice, $Processor, $Memory, $Storage);
+                $preparedQuery2->execute();
+                $preparedQuery2->store_result();
+                $preparedQuery2->bind_result($newserviceid);
 
-                if($preparedQuery->fetch()){ //Found a match, update ServerTable and use this type instead
+                if($preparedQuery2->fetch()){ //Found a match, update userservers and use this type instead
                     //Pretty much do nothing
                 }
                 else{ //No match found, create new type
-                    //Get the max value from the serverType column of the ServicesTable
-                    if($preparedQuery = $conn->prepare('SELECT MAX(serverType) FROM servicesTable')){
-                        $preparedQuery->execute();
-                        $preparedQuery->bind_result($maxType);
+                    //Get the max value from the serviceid column of the nimbusioservices
+                    if($preparedQuery3 = $conn->prepare('SELECT MAX(serviceid) FROM nimbusioservices')){
+                        $preparedQuery3->execute();
+                        $preparedQuery3->store_result();
+                        $preparedQuery3->bind_result($maxType);
 
-                        $newServerType = $maxType + 1;
+                        $newserviceid = $maxType + 1;
 
-                        //Insert new type record into ServicesTable, we want serverType to be set 
+                        //Insert new type record into nimbusioservices, we want serviceid to be set 
                         //  to AUTO-INCREMENT on the backend-side for new records that are inserted
-                        //  into the table of the ServicesTable. Ideally, this incremented record should
-                        //  match the value of the variable $newServerType at this point
-                        if($preparedQuery = $conn->prepare('INSERT INTO servicesTable (serverName, serverDesc, serverProc, serverRAM, serverSpace) VALUES (?,?,?,?,?)')){
-                            $preparedQuery->bind_param('sssss', $serverName, $serverDesc, $serverProc, $serverRAM, $serverSpace);
-                            $preparedQuery->execute();
+                        //  into the table of the nimbusioservices. Ideally, this incremented record should
+                        //  match the value of the variable $newserviceid at this point
+                        if($preparedQuery4 = $conn->prepare('INSERT INTO nimbusioservices (servicename, servicedesc, serviceprice, Processor, Memory, Storage) VALUES (?,?,?,?,?)')){
+                            $preparedQuery4->bind_param('ssisii', $servicename, $servicedesc, $serviceprice, $Processor, $Memory, $Storage);
+                            $preparedQuery4->execute();
+
+                            $preparedQuery4->close();
                         }
+                        $preparedQuery3->close();
                     }
                 }
+                $preparedQuery2->close();
 
-                //Now we update ServerTable to reflect changes for custom type
-                    //UPDATE serverTable SET serverType=$newServerType WHERE uid=$uid AND serverType=serverType
-                if($preparedQuery = $conn->prepare('UPDATE serverTable SET serverType=? WHERE uid=? AND serverType=?')){
-                        $preparedQuery->bind_param('iii', $newServerType, $uid, $serverType)
-                        $preparedQuery->execute();
+                //Now we update userservers to reflect changes for custom type
+                    //UPDATE userservers SET serviceid=$newserviceid WHERE uid=$uid AND serviceid=serviceid
+                if($preparedQuery5 = $conn->prepare('UPDATE userservers SET serviceid=? WHERE uid=? AND serviceid=?')){
+                    $preparedQuery5->bind_param('iii', $newserviceid, $uid, $serviceid)
+                    $preparedQuery5->execute();
+
+                    $preparedQuery5->close();
                 }
             }
         }
