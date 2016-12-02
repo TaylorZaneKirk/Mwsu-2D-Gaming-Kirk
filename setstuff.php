71004e7b49863f6828c23f8144be88677f1a5aa2
Taylor Kirk <TaylorZaneKirk@gmail.com>
2016-12-02 12:01:37 -0600

    test

--- setstuff.php
+++ setstuff.php
@@ -1,32 +1,48 @@
 <?php
     require "conn.php";
-    echo "test-1";
-    //$uid = $_GET['uid'];
+
+    $uid = $_GET['uid'];
     //$IPaddress = $_GET['IPaddress'];
     //$serviceid = $_GET['serviceid'];
-    //$servicename = $_GET['servicename'];
-    //$servicedesc = $_GET['servicedesc'];
-    //$serviceprice = $_GET['serviceprice'];
-    //$Memory = $_GET['Memory'];
-    //$Processor = $_GET['Processor'];
-    //$Storage = $_GET['Storage'];
+    $servicename = $_GET['servicename'];
+    $servicedesc = $_GET['servicedesc'];
+    $serviceprice = 4.99;
+    $Memory = $_GET['Memory'];
+    $Processor = $_GET['Processor'];
+    $Storage = $_GET['Storage'];
 
     $uid = 2;
-    //$IPaddress = $_GET['IPaddress'];
-    $serviceid = 1;
-    $servicename = "LAMP";
-    $servicedesc = "Linux, Apache, MySQL, PHP";
-    $serviceprice = 4.99;
+    $servicename = "blah";
+    $servicedesc = "test";
     $Memory = 512;
     $Processor = "Xenon";
-    $Storage = 10240;
+    $Storage = 10000;
 
     //generate a random IPaddress string
     $IPaddress = "".mt_rand(0,255).".".mt_rand(0,255).".".mt_rand(0,255).".".mt_rand(0,255);
+
+    if($preparedQuery3 = $conn->prepare('SELECT MAX(serviceid) FROM nimbusioservices')){
+        $preparedQuery3->execute();
+        $preparedQuery3->store_result();
+        $preparedQuery3->bind_result($maxType);
+        $preparedQuery3->fetch();
+        $serviceid = $maxType + 1;
+        $preparedQuery3->close();
+    }
+
+    //Insert new type record into nimbusioservices, we want serviceid to be set 
+    //  to AUTO-INCREMENT on the backend-side for new records that are inserted
+    //  into the table of the nimbusioservices. Ideally, this incremented record should
+    //  match the value of the variable $newserviceid at this point
+    if($preparedQuery4 = $conn->prepare('INSERT INTO nimbusioservices (serviceid, servicename, servicedesc, serviceprice, Processor, Memory, Storage) VALUES (?,?,?,?,?,?,?)')){
+        $preparedQuery4->bind_param('issdsii', $serviceid, $servicename, $servicedesc, $serviceprice, $Processor, $Memory, $Storage);
+        $preparedQuery4->execute();
+        $preparedQuery4->store_result();
+        $preparedQuery4->close();
+    }
     
-    echo "test0 " . $uid . " " . $serviceid . " " . $IPaddress;
     //User just bought a server, add their server to userservers
-    if($preparedQuery = $conn->prepare('INSERT INTO userservers (uid, serviceid, IPaddress) VALUES (?, ?, ?)')){
+    if($preparedQuery = $conn->prepare('INSERT INTO userservers (uid, serverid, IPaddress) VALUES (?, ?, ?)')){
         $preparedQuery->bind_param('iis', $uid, $serviceid, $IPaddress);
         $preparedQuery->execute();
         //If using preset types, job is done. If user is using a custom type, first
@@ -37,8 +53,6 @@
         //  and then update the serviceid record on the userservers to reflect
         //  the new serviceid
         $preparedQuery->store_result();
-        echo "test1";
+        $preparedQuery->close();          
     }
-        
-    
 ?>
